3bd667745f66f369b0776424be31791a2b076cf6
Hubert Hartan <hubert.hartan@students.mq.edu.au>
2021-08-17 14:36:38 +1000

    Added base stuff to poem component

--- src/Components/poem.js
+++ src/Components/poem.js
@@ -1,7 +1,12 @@
 import React,{useState} from 'react';
 
 const poem = ({poem}) =>{
-    
+    return(
+        <p>{poem.title}</p>,
+        <p>{poem.author}</p>,
+        <p>{poem.text}</p>,
+        <p>{poem.votes}</p>
+    )
 
 }
 
