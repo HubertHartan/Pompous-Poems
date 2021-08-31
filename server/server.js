3e0dee29c9ac681e0f8f3eab50317348cfe673ca
Hubert Hartan <hubert.hartan@students.mq.edu.au>
2021-08-31 15:46:27 +1000

    updated server backend

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const fs = require("fs") 
 
 // Load data from JSON file into memory
-const rawData = fs.readFileSync("server/units.json")
+const rawData = fs.readFileSync("server/poems.json")
 const data = JSON.parse(rawData)
 
 const app = express() 
@@ -11,34 +11,45 @@ app.use(cors())
 app.use(express.json()) 
 
 
-app.get('/api/units', (req, res) => {
+app.get('/api/poems', (req, res) => {
     res.json(data.units)
 })
 
-app.post('/api/units', (req, res) => {
+app.get('/api/poems/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const unit = data.units.filter(u => u.id === id)[0]
+  // return a 404 if there is no such poem
+  if (unit) {
+    res.json(unit)
+  } else {
+    res.status(404)
+    res.send("<h1>Poem not found.</h1>")
+  }
+})
+
+
+app.post('/api/poems', (req, res) => {
   const body = req.body
   console.log(body)
-  const newUnit = {
+  const newPoem = {
       title: body.title,
       code: body.code,
       offering: body.offering,
       id: data.units.length   
   }
-  data.units.push(newUnit) 
-  res.json(newUnit)
+  data.units.push(newPoem) 
+  res.json(newPoem)
 })
 
-app.get('/api/units/:id', (req, res) => {
+app.post('/api/units/:id', (req, res) => {
+  const newPoem = req.body
   const id = Number(req.params.id)
-  const unit = data.units.filter(u => u.id === id)[0]
-  // return a 404 if there is no such unit
-  if (unit) {
-    res.json(unit)
-  } else {
-    res.status(404)
-    res.send("<h1>Unit not found.</h1>")
-  }
+  data.units = data.units.map(e => id === e.id ? newPoem : e)
+  console.log("updated", newPoem)
+  res.json(newPoem)
 })
+
+
 
 app.delete('/api/units/:id', (req, res) => {
   const id = Number(req.params.id)
@@ -49,17 +60,10 @@ app.delete('/api/units/:id', (req, res) => {
     res.json("deleted")
   } else {
     res.status(404)
-    res.send("<h1>Unit not found</h1>")
+    res.send("<h1>Poem not found</h1>")
   }
 })
 
-app.put('/api/units/:id', (req, res) => {
-  const newUnit = req.body
-  const id = Number(req.params.id)
-  data.units = data.units.map(e => id === e.id ? newUnit : e)
-  console.log("updated", newUnit)
-  res.json(newUnit)
-})
 
 
 const PORT = 3001
