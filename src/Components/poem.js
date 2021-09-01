38d0850a4ac97c7bb07c9515ea7d02bf04a5fdd9
Hubert Hartan <hubert.hartan@students.mq.edu.au>
2021-09-01 12:42:13 +1000

    change poem component

--- src/Components/poem.js
+++ src/Components/poem.js
@@ -1,12 +1,42 @@
 import React,{useState} from 'react';
+import PoemForm from './PoemForm.js'
+
 
 const Poem = ({poem}) =>{
-    return(
-        <p>{poem.title}</p>,
-        <p>{poem.author}</p>,
-        <p>{poem.text}</p>,
-        <p>{poem.votes}</p>
-    )
+    const [editing, setEditing] = useState(false)
+
+    const editAction = () => {
+      setEditing(true)
+    }
+    
+    const finaliseEdit =  (newUnit) => {
+      console.log(newUnit)
+      setEditing(false)
+      updateFn(newUnit)
+    }
+  
+    const cancelEdit = () => {
+      setEditing(false)
+    }
+  
+    if (editing) {
+      return (
+        <li>
+        <UnitForm updateFn={finaliseEdit} unitInfo={unit} />
+        <button onClick={cancelEdit}>Cancel Edit</button>
+        </li>
+      )
+    } else {
+    return (
+        <li>{poem.author}: 
+        {poem.title} 
+        {opem.text}
+        <button onClick={() => deleteFn(unit)}>Delete</button>
+        <button onClick={editAction}>Edit</button>
+        </li>
+      )
+    }
+
 
 }
 
